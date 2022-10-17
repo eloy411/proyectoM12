51d567e7ae1a49f84a51f59b2f7f5b5ce67baf8b
eloy411 <91909651+eloy411@users.noreply.github.com>
2022-10-17 14:50:51 +0200

    prueba definitiva

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,4 +1,5 @@
 const express=require('express')
+const Preguntas= require('../models/preguntas')
 const router=express.Router()
 
 const {setPreguntas,renderAdmin} = require('../controllers/admin.controller')
@@ -7,6 +8,14 @@ const {setPreguntas,renderAdmin} = require('../controllers/admin.controller')
 
 router.get('/admin',renderAdmin)
 router.post('/admin/add-preguntas',setPreguntas)
+
+router.get('/admin/get',async (req,res)=>{
+
+    const preguntas = await Preguntas.find()
+
+    res.send(preguntas)
+
+})
 
 console.log("route admin")
 module.exports = router
